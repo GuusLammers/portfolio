c1091d8dc8f43c728bb1044a3dd28373adf8d945
GuusLammers <guuslammers16@gmail.com>
2023-11-18 09:46:59 -0800

    Add icon and change title

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,14 @@
 import { ParticlesBackground } from './components/particles/ParticlesBackground';
 import { ThemeProvider } from '@mui/material/styles';
 import { createMuiTheme } from './theme';
+import { Helmet } from 'react-helmet';
 
 const App = () => {
   return (
     <ThemeProvider theme={createMuiTheme()}>
+      <Helmet>
+        <title>Portfolio</title>
+      </Helmet>
       <ParticlesBackground />
     </ThemeProvider>
   );
