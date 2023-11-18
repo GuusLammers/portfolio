13464506cf507c68aae712f37e4ee6ecac20ad2f
GuusLammers <guuslammers16@gmail.com>
2023-11-18 09:38:43 -0800

    Extracted particles into own component and added mui theme

---
+++ src/theme/index.ts
@@ -0,0 +1,19 @@
+import { createTheme } from '@mui/material/styles';
+
+export const createMuiTheme = () => {
+  const theme = createTheme({
+    palette: {
+      primary: {
+        main: '#20c20e',
+      },
+      secondary: {
+        main: '#2fbace',
+      },
+      background: {
+        default: '#000000',
+      },
+    },
+  });
+
+  return theme;
+};
