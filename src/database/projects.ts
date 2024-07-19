ec22d04c5908dee1f240a8a35c3738fe8babf8f0
GuusLammers <guuslammers16@gmail.com>
2024-07-18 20:01:07 -0700

    Added project content

--- src/database/projects.ts
+++ src/database/projects.ts
@@ -2,13 +2,39 @@ import { TimelineObject } from '../types/timeline';
 
 export const PROJECTS: TimelineObject[] = [
   {
+    title: 'GL Line Indicator',
+    subTitle: 'Pinescript',
+    timeRange: 'June 2024',
+    image: 'tradingview.png',
+    content: {
+      listItems: [
+        'Designed a TradingView indicator for traders to identify market trends across multiple timeframes using moving averages and Average True Range (ATR), featuring customizable parameters, color-coded trend indicators, and a confluence table for clearer decision-making, which I actively use for my crypto trading.',
+      ],
+    },
+  },
+  {
+    title: 'GL Gann Swing Indicator',
+    subTitle: 'Pinescript',
+    timeRange: 'February 2024',
+    image: 'tradingview.png',
+    content: {
+      listItems: [
+        "Designed a TradingView indicator featuring W.D. Gann's Swing Chart principles, bar type filters, visual indicators, break lines, bar count analysis, and customizable moving averages for comprehensive trend analysis and trading strategy optimization, which I actively use for my crypto trading.",
+      ],
+    },
+  },
+  {
     title: 'Porfolio V2',
     subTitle: 'Vite + TypeScript + React',
     timeRange: 'January 2024',
     image: 'icon.ico',
     githubLink: 'https://github.com/guuslammers/portfolio',
     githubPagesLink: 'https://guuslammers.github.io/portfolio/about',
-    content: {},
+    content: {
+      listItems: [
+        'A Single Page Application (SPA) built using TypeScript and React to display my personal projects and experience, featuring an integrated interactive particle background for enhanced user experience.',
+      ],
+    },
   },
   {
     title: 'Evolutionary Algorithm Visualizer',
@@ -16,7 +42,11 @@ export const PROJECTS: TimelineObject[] = [
     timeRange: 'October 2022',
     image: 'evolution.png',
     githubLink: 'https://github.com/GuusLammers/Evolutionary-Algorithm-Visualizer',
-    content: {},
+    content: {
+      listItems: [
+        'An interactive tool used to visualize an evolutionary algorithm implemented using the MVC design pattern. Working on this project allowed me to refine my skills using OOP, the MVC design pattern, and Java Swing components and event systems. By implementing this project using the MVC design pattern, I was able to decouple the view from the model, improving the cod organization and readability. This project also improved my knowledge of how evolutionary algorithms work.',
+      ],
+    },
   },
   {
     title: 'Porfolio V1',
@@ -25,7 +55,11 @@ export const PROJECTS: TimelineObject[] = [
     image: 'icon.ico',
     githubLink: 'https://github.com/GuusLammers/My-Portfolio-Website',
     githubPagesLink: 'https://guuslammers.github.io/My-Portfolio-Website/',
-    content: {},
+    content: {
+      listItems: [
+        'A SPA built using Vanilla JavaScript and Three.js to display my personal programming projects. Working on this project allowed me to refine my skills using JavaScript, Three.js, HTML, and CSS. This portfolio is being hosted using GitHub Pages.',
+      ],
+    },
   },
   {
     title: '2048',
@@ -33,7 +67,11 @@ export const PROJECTS: TimelineObject[] = [
     timeRange: 'September 2022',
     image: '2048.png',
     githubLink: 'https://github.com/GuusLammers/2048',
-    content: {},
+    content: {
+      listItems: [
+        'A simple implementation of the popular game 2048 implemented using the MVC design pattern. Working on this project allowed me to refine my skills using OOP, the MVC design pattern, and Java Swing components and event systems. By implementing this project using the MVC design pattern, I was able to decouple the view from the model, improving the code organization and readability.',
+      ],
+    },
   },
   {
     title: 'Sorting Algorithm Visualizer',
@@ -41,7 +79,11 @@ export const PROJECTS: TimelineObject[] = [
     timeRange: 'April 2022',
     image: 'sort.png',
     githubLink: 'https://github.com/GuusLammers/Sorting-Algorithm-Visualizer',
-    content: {},
+    content: {
+      listItems: [
+        'A sorting algorithm visualizer implemented using Tkinter and the MVC design pattern using a custom events system. The visualizer implements visualizations for selection sort, merge sort, quick sort, insertion sort, and cocktail sort. Working on this project allowed me to refine my skills using Tkinter, OOP, the MVC design pattern, and gave me the opportunity to create and implement my own custom event system.',
+      ],
+    },
   },
   {
     title: 'Snake',
@@ -49,7 +91,11 @@ export const PROJECTS: TimelineObject[] = [
     timeRange: 'April 2022',
     image: 'snake.png',
     githubLink: 'https://github.com/GuusLammers/Snake',
-    content: {},
+    content: {
+      listItems: [
+        'A simple snake game implemented using the MVC design pattern. Working on this project allowed me to refine my skills using OOP, the MVC design pattern, and Java Swing components and event systems. By implementing this project using the MVC design pattern, I was able to decouple the view from the model, improving the code organization and readability.',
+      ],
+    },
   },
   {
     title: 'Flappy Bird Clone',
@@ -57,7 +103,11 @@ export const PROJECTS: TimelineObject[] = [
     timeRange: 'January 2021',
     image: 'flappy-bird.png',
     githubLink: 'https://github.com/GuusLammers/Flappy-Bird-Clone',
-    content: {},
+    content: {
+      listItems: [
+        'A clone of the popular game Flappy Bird implemented using Pygame. Working on this project introduced me to simple 2D game development using Pygame.',
+      ],
+    },
   },
   {
     title: 'Path Finder',
@@ -65,7 +115,11 @@ export const PROJECTS: TimelineObject[] = [
     timeRange: 'December 2020',
     image: 'path-finder.png',
     githubLink: 'https://github.com/GuusLammers/Pathfinder',
-    content: {},
+    content: {
+      listItems: [
+        'An interactive visualization tool that visually compares path-finding algorithms. This tool allows the user to create the visualization setup by placing the start position, end position, and any obstacles. The visualizer implements visulizations for Dijkstras, A*, and Best First Search path finding algorithms. Working on this project refined my knowledge of the underlying mechanics of path finding algorithms and the effects heuristics can have on the algorithms efficiency.',
+      ],
+    },
   },
   {
     title: 'Spiral Sort',
@@ -73,7 +127,11 @@ export const PROJECTS: TimelineObject[] = [
     timeRange: 'December 2020',
     image: 'spiral.png',
     githubLink: 'https://github.com/GuusLammers/Spiral-Sort',
-    content: {},
+    content: {
+      listItems: [
+        'A sorting algorithm visualizer implemented using Pygame that scatters and reconstructs a spiral. The visualizer implements visualizations for selection sort, merge sort, quick sort, insertion sort, cocktail sort, bubble sort, shell sort, and comb sort. Working on this project allowed me to refine my skills using Pygame and my knowledge of sorting algorithms.',
+      ],
+    },
   },
   {
     title: 'Cimex',
@@ -81,7 +139,11 @@ export const PROJECTS: TimelineObject[] = [
     timeRange: 'November 2020',
     image: 'bug.png',
     githubLink: 'https://github.com/GuusLammers/BugTracker-Flask-WebApp',
-    content: {},
+    content: {
+      listItems: [
+        'A bug-tracking web application called Cimex that I created to track my projects. By working on this project I gained a thorough understanding of full stack web development, by designing a relational database, a RESTful API, and a visually aesthetic front-end. I implemented tbe application with complete authorization/authentication functionality.',
+      ],
+    },
   },
   {
     title: 'Sudoku',
@@ -89,7 +151,11 @@ export const PROJECTS: TimelineObject[] = [
     timeRange: 'October 2020',
     image: 'sudoku.png',
     githubLink: 'https://github.com/GuusLammers/GUI-Sudoku-Solver',
-    content: {},
+    content: {
+      listItems: [
+        'A simple sudoku game implemented using Tkinter. The game can be solved by the user or with the implemented recursive back tracking algorithm.',
+      ],
+    },
   },
   {
     title: 'Blackjack',
@@ -97,6 +163,8 @@ export const PROJECTS: TimelineObject[] = [
     timeRange: 'October 2020',
     image: 'blackjack.png',
     githubLink: 'https://github.com/GuusLammers/GUI-Blackjack',
-    content: {},
+    content: {
+      listItems: ['A simple blackjack game implemented using Tkinter.'],
+    },
   },
 ];
